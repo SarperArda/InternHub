6cc23e43c2f14f6d5225da62a12d3bdd93e82cfb
Deniz Tuna Onguner <tuna.onguner@ug.bilkent.edu.tr>
2023-04-28 19:08:06 +0300

    Delete current project

--- InternHub/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_page, name='login'),
-    path('home/', views.home_page, name='home'),
-    path('logout/', views.logout_page, name='logout'),
-]
