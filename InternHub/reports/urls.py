953f50edd3e6195d9e80a73f3ea3283a6842f757
Sarper Arda Bakır <arda.bakir@ug.bilkent.edu.tr>
2023-05-11 20:39:32 +0300

    SummerTrainingGradingForm
    I try to write SummerTrainingGradingForm on forms,urls,views on reports. Form and url should be done but views and models is not.

--- InternHub/reports/urls.py
+++ InternHub/reports/urls.py
@@ -4,4 +4,5 @@ app_name = 'reports'
 
 urlpatterns = [
     path('create-confidential-form/', views.CreateConfidentialForm.as_view(), name='login'),
+    path('create-summer-training-form/', views.SummerTrainingGradingForm.as_view(), name='login'),
 ]
--- InternHub/reports/views.py
+++ InternHub/reports/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from reports.forms import ConfidentialCompanyForm
+from reports.forms import SummerTrainingGradingForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Internship
 from users.models import Student
@@ -39,4 +40,33 @@ class CreateConfidentialForm(LoginRequiredMixin, FormView):
         # If there are no errors, save the form and update the internship status.
         form.save()
         internship.confidential_company_form.status = status
+        return super().form_valid(form)
+
+#not completed
+class SummerTrainingGradingForm(LoginRequiredMixin, FormView):
+    form_class = SummerTrainingGradingForm
+    template_name = 'reports/create_summer_training_form.html'
+    success_url = '/student/students/' # not good name
+
+    def form_valid(self, form):
+        # Checking if the report is satisfactory.
+
+        if form.cleaned_data['sum_score_evaluation_except_one'] < 7:
+            status = 'REJECTED'
+        elif form.cleaned_data['sum_score_evaluation_except_one'] < 30:
+            status = 'REJECTED'
+        elif not form.cleaned_data['score_evaluation_report']:
+            status = 'REJECTED'
+        else:
+            status = 'ACCEPTED'
+
+        # Getting the user_id from the form.
+        user_id = form.cleaned_data['student_id']
+    
+        # If the form is not valid, return the form with the errors.
+        if form.errors != {}:
+            return super().form_invalid(form)
+
+        # If there are no errors, save the form.
+        form.save()
         return super().form_valid(form)
