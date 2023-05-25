3320d6d2bdbcd191b4546616985fe9cbd2f6bbb5
Sarper Arda Bakır <arda.bakir@ug.bilkent.edu.tr>
2023-05-25 20:40:36 +0300

    User role check

--- InternHub/main/views.py
+++ InternHub/main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from announcements.models import Announcement
+from reports.models import Internship
 
 
 class HomeView(LoginRequiredMixin, View):
@@ -11,4 +12,10 @@ class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         announcements = Announcement.objects.all()
         full_name = str(request.user)
+        user = request.user
+        id = request.user.user_id
+        if(user.role == 'STUDENT' or user.role == 'INSTRUCTOR'):
+            check = True
+            internship = Internship.objects.filter(student_id=id)
+            return render(request, 'main/home.html', {'announcements': announcements, 'full_name': full_name, 'user': user, 'internship': internship, 'check': check})
         return render(request, 'main/home.html', {'announcements': announcements, 'full_name': full_name})
